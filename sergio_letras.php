ec647bb80ac29a15d27fa164a3402a60268d13ca
SergioPenaCancelo1 <sergiomolademasiado@gmail.com>
2020-03-01 18:47:45 +0100

    Avances importantes del plugin
    La letra ahora se muestra bien ordenada por versos haciendo los saltos de linea en los lugares apropiados .
    Puesto el formulario para poder buscar las canciones manualmente  a  insertando el codigo HTML directamente  en la pagina mediante una variable en un shortcode.
    Llamada a la api modificada para que funcione con el formulario y arreglado el error que salia al escribir el artista o la canción con espacios sustituyendolos por "%20"

--- sergio_letras.php
+++ sergio_letras.php
@@ -14,27 +14,70 @@ add_action('init','sergio_registrer_shortcodes');
 
 	function sergio_registrer_shortcodes() {
 			add_shortcode('letra','obtener_letra');
+      add_shortcode('formulario','realizar_formulario');
 	}
 
-	function obtener_letra($args,$content){
-          // return "esto es una prueba del plugin de letras";
 
-           //return $content.' '.$args['cancion'].' '.$args['artista'] ;
 
-           //$result = wp_remote_get('La URL'.$args['cancion'].$args['artista'].'continuacion de la URL');
+  function realizar_formulario($args){
+           $formulario='<form action="" method="post">
+                <label><b>Dinos tu artista favorito</b></label> 
+                <p>Artista: <input type="text" name="artista"></p>
+                <p>Canción: <input type="text" name="cancion"></p>
+                <p>
+                  <input type="submit" value="Enviar">
+                  <input type="reset" value="Borrar">
+                </p>
+            </form>';
 
+            return $formulario;
 
             
 
-        
-            //$result = wp_remote_get('https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback&q_track=iron%20maiden&q_artist=iron%20maiden&apikey=f37bb1f59ec257857a2086c7bdb4a1ea');
+  }
 
-            $result = wp_remote_get('https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback&q_track='.$args['cancion'].'.&q_artist='.$args['artista'].'&apikey=f37bb1f59ec257857a2086c7bdb4a1ea');
+	function obtener_letra($args,$content){
+
+                /*$formulario='<div><form action="" method="post">
+                <label>Dinos tu artista favorito</label> 
+                <p>Artista: <input type="text" name="artista"></p>
+                <p>Canción: <input type="text" name="cancion"></p>
+                <p>
+                  <input type="submit" value="Enviar">
+                  <input type="reset" value="Borrar">
+                </p>
+            </form></div>';
+
+                echo $formulario;*/
+
+              $artista= $_POST['artista'];
+
+              $cancion= $_POST['cancion'];
+
+              $artista_noespacios = str_replace(" ", "%20", $artista);
+
+              $cancion_noespacios = str_replace(" ", "%20", $cancion);
+
+             
+            //$url="https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback&q_track=".$args['cancion'].".&q_artist=".$args['artista']."&apikey=f37bb1f59ec257857a2086c7bdb4a1ea";
+
+            $url="https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback&q_track=".$cancion_noespacios.".&q_artist=".$artista_noespacios."&apikey=f37bb1f59ec257857a2086c7bdb4a1ea";
+          
+            $result = file_get_contents($url);
+              
+            $datos = json_decode($result,true);
 
 
-           return $result['body'].' '.esc_attr($content);
+            $letra = $datos['message']['body']['lyrics']['lyrics_body'];
 
-           
+            $letra_ordenada = str_replace("\n", "</br>", $letra);
+
+
+
+
+            return  "<div><p><strong>".$cancion."-".$artista."</strong></p><p>".$letra_ordenada."</p></div>";
+           // return $datos['message']['body']['lyrics']['lyrics_body'];
+                      
 	}
 
 ?>
